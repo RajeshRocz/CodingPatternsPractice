0556593398f2ae0dcbf7677a489fb4b9604485dc
RajeshRocz <rrajeshelite@gmail.com>
2025-01-23 22:08:45 -0600

    Stack/ValidParenthesisString

---
+++ src/Stack/ValidParenthesisString.java
@@ -0,0 +1,53 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ValidParenthesisString {
+    public static void main(String[] args) {
+        System.out.println("Result:" + isValidString("((()*))("));
+
+    }
+
+    /*
+    Input: s = "(*))"
+    Output: true
+     */
+    private static boolean isValidString(String s) {
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                open.push(i);
+            } else if (s.charAt(i) == '*') {
+                star.push(i);
+            } else {
+
+                if (!open.isEmpty()) {
+                    open.pop();
+                } else if (!star.isEmpty()) {
+                    star.pop();
+                } else {
+                    return false;
+                }
+            }
+
+        }
+
+        //Now do left over open braces
+        while (!open.isEmpty()) {
+            if (star.isEmpty()) {
+                return false;
+            } else if (open.peek() < star.peek()) {
+                open.pop();
+                star.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
